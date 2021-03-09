340be5a7e51fb70cf7157c7df7552099a7ecdde8
sagheerali1008 <alisahheer1008@gmail.com>
2021-03-09 12:17:20 +0400

    Updated interface contracts

--- contracts/interfaces/IStrategy.sol
+++ contracts/interfaces/IStrategy.sol
@@ -2,19 +2,48 @@
 pragma solidity ^0.8.0;
 
 interface IStrategy {
-    function want() external view returns (address);
+    function getWant() external view returns (address);
 
     function deposit() external;
 
     // Controller | Vault role - withdraw should always return to Vault
     function withdraw(uint256) external;
 
+    // Controller role - withdraw should always return to recipient wallet directly
+    function withdraw(address, uint256) external;
+
     // Controller | Vault role - withdraw should always return to Vault
     function withdrawAll() external returns (uint256);
+
+    function withdrawAsWault(address, uint256) external returns (uint256);
+
+    function harvest() external returns (uint256);
 
     function balanceOf() external view returns (uint256);
 
     function withdrawalFee() external view returns (uint256);
     
     function supplyRatePerBlock() external view returns (uint256);
+
+    function borrowRatePerBlock() external view returns (uint256);
+
+    function supplyXvsRatePerBlock() external returns (uint256);
+    
+    function borrowXvsRatePerBlock() external returns (uint256);
+
+    function totalSupplyApy() external returns (uint256);
+
+    function totalApy() external returns (uint256);
+
+    function venusSpeeds() external returns (uint256, uint256, uint256);
+
+    function borrowLimit() external view returns (uint256);
+
+    function totalVTokenSupply() external view returns (uint256);
+
+    function totalVTokenBorrows() external returns (uint256);
+
+    function pause() external;
+
+    function unpause() external;
 }
--- contracts/interfaces/IVToken.sol
+++ contracts/interfaces/IVToken.sol
@@ -150,17 +150,13 @@ abstract contract VTokenInterface is VTokenStorage {
     function transferFrom(address src, address dst, uint amount) external virtual returns (bool);
     function approve(address spender, uint amount) external virtual returns (bool);
     function allowance(address owner, address spender) external virtual view returns (uint);
-    function balanceOf(address owner) external virtual view returns (uint);
-    function balanceOfUnderlying(address owner) external virtual returns (uint);
     function getAccountSnapshot(address account) external virtual view returns (uint, uint, uint, uint);
     function borrowRatePerBlock() external virtual view returns (uint);
     function supplyRatePerBlock() external virtual view returns (uint);
     function totalBorrowsCurrent() external virtual returns (uint);
-    function borrowBalanceCurrent(address account) external virtual returns (uint);
     function borrowBalanceStored(address account) external virtual view returns (uint);
     function exchangeRateCurrent() external virtual returns (uint);
     function exchangeRateStored() external virtual view returns (uint);
-    function getCash() external virtual view returns (uint);
     function accrueInterest() external virtual returns (uint);
     function seize(address liquidator, address borrower, uint seizeTokens) external virtual returns (uint);
 
@@ -197,7 +193,10 @@ abstract contract VBep20Interface is VBep20Storage {
         uint repayAmount,
         VTokenInterface vTokenCollateral
     ) external virtual returns (uint);
-
+    function getCash() external virtual view returns (uint);
+    function balanceOf(address owner) external virtual view returns (uint);
+    function borrowBalanceCurrent(address account) external virtual returns (uint);
+    function balanceOfUnderlying(address owner) external virtual returns (uint);
 
     /*** Admin Functions ***/
     function _addReserves(uint addAmount) external virtual returns (uint);
